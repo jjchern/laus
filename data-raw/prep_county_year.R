32cab68cd4ec21760178f97c6f17bacba32654bc
jjchern <jiajia.chern@gmail.com>
2018-01-03 16:08:19 -0600

    Update county annual series (up to 2016)

--- data-raw/prep_county_year.R
+++ data-raw/prep_county_year.R
@@ -1,33 +1,51 @@
-# Labor force data by county, annual averages, 1990-2015
+# Labor force data by county, annual averages, 1990-2016
 
 library(tidyverse)
 
 # Download all Excel files ------------------------------------------------
 
-urls = paste0("https://www.bls.gov/lau/laucnty", c(90:99, formatC(0:15, width=2, flag=0)), ".xlsx")
-fils = paste0("data-raw/", basename(urls))
+urls = paste0("https://www.bls.gov/lau/laucnty", c(90:99, formatC(0:16, width=2, flag=0)), ".xlsx")
+fils = paste0("data-raw/county_year/", basename(urls))
 map2(urls, fils, download.file)
+
+# Check out a few files ---------------------------------------------------
+
+fils[1] %>%
+    readxl::read_excel(col_names = FALSE, skip = 5) %>%
+    select(-X__6) %>%         # Drop An empty column
+    filter(X__2 != "NA") %>%  # Drop the last few rows
+    rename(laus_code = X__1,
+           state_fips = X__2,
+           county_fips = X__3,
+           county = X__4,
+           year = X__5,
+           labor_force = X__7,
+           employed = X__8,
+           unemployed = X__9,
+           unemployment_rate = X__10) %>%
+    separate(county, c("county", "state"), sep = ", ") %>% # Doesn't work for DC
+    unite(fips, state_fips, county_fips, sep = "", remove = FALSE)
+
+# Error message in row 317
+readxl::read_excel("data-raw/county_year/laucnty90.xlsx", col_names = FALSE, skip = 5)[317,]
 
 # Load all Excel files ----------------------------------------------------
 
-readxl::read_excel("data-raw/laucnty90.xlsx", col_names = FALSE, skip = 5)[317,]
-
 tidy <- . %>%
-        readxl::read_excel(col_names = FALSE, skip = 5) %>%
-        select(-X5) %>%             # Drop An empty column
-        filter(X1 != "NA") %>%      # Drop the last four rows
-        rename(laus_code = X0,
-               state_fips = X1,
-               county_fips = X2,
-               county = X3,
-               year = X4,
-               labor_force = X6,
-               employed = X7,
-               unemployed = X8,
-               unemployment_rate = X9) %>%
-        separate(county, c("county", "state"), sep = ", ") %>% # Doesn't work for DC
-        mutate(state = if_else(county == "District of Columbia", "DC", state)) %>%
-        unite(fips, state_fips, county_fips, sep = "", remove = FALSE)
+    readxl::read_excel(col_names = FALSE, skip = 5) %>%
+    select(-X__6) %>%         # Drop An empty column
+    filter(X__2 != "NA") %>%  # Drop the last few rows
+    rename(laus_code = X__1,
+           state_fips = X__2,
+           county_fips = X__3,
+           county = X__4,
+           year = X__5,
+           labor_force = X__7,
+           employed = X__8,
+           unemployed = X__9,
+           unemployment_rate = X__10) %>%
+    separate(county, c("county", "state"), sep = ", ") %>% # Doesn't work for DC
+    unite(fips, state_fips, county_fips, sep = "", remove = FALSE)
 
 map(fils, tidy) %>% map(head) # Warnings are either about DC or missing values
 
@@ -37,4 +55,3 @@ county_year
 # Save it! ----------------------------------------------------------------
 
 devtools::use_data(county_year, overwrite = TRUE)
-unlink(fils)
